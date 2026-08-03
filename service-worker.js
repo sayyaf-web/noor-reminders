87ee47c6aad08ec8e775abf7f4e896c923b9fcfc
Sayyaf <athmanbilal16@gmail.com>
2026-08-03 09:58:40 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,68 @@
-const CACHE_NAME = "noor-reminders-v2";
+const CACHE_NAME = "noorly-v1.1";
 
 const urlsToCache = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./privacy.html",
+  "./Icons/icon-192-1.png",
+  "./Icons/icon-512-1.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activate
 self.addEventListener("activate", event => {
   event.waitUntil(
-    Promise.all([
-      self.clients.claim(),
-      caches.keys().then(keys =>
-        Promise.all(
-          keys.map(key => {
-            if (key !== CACHE_NAME) {
-              return caches.delete(key);
-            }
-          })
-        )
-      )
-    ])
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
+// Fetch (Cache First, then Network)
 self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(cached => {
+
+        if (cached) {
+          return cached;
+        }
+
+        return fetch(event.request)
+          .then(response => {
+
+            const copy = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, copy));
+
+            return response;
+
+          })
+          .catch(() => caches.match("./index.html"));
+
+      })
   );
+
 });
